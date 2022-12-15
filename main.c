f86fa00294ce6befd880627965f7f973b1f3c9ff
Leibnitz Edison Andres Rojas Castillo <leibnitz.rojas@uptc.edu.co>
2022-12-15 14:59:52 -0500

    Agregamos While

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1; i<=7; i++){
-        printf("Hello, World!\n");
+    for (int i = 1; i<=20; i++){
+        printf("Hello mundo!\n");
     }
 
     return 0;
